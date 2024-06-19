78b75b2303962669478810405d45120016467e9a
Andrew Bernas <andrewrbernas@gmail.com>
2024-06-19 14:46:28 -0400

    Created mocap feature in control node and test

---
+++ scripts/mocap.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import tf
+import rospy
+from control_node import MavController
+
+
+def simple_demo():
+    """
+    A simple demonstration of using mavros commands to control a UAV.
+    """
+    # Setpoint publishing MUST be faster than 2Hz
+    rate = 20
+
+    c = MavController(rate)
+
+    while True:
+        print(c.pose)
+
+
+if __name__ == "__main__":
+    simple_demo()
+
