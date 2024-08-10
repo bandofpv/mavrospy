f2c7efefe197ec6c53e85d153ba013e1504472ed
bandofpv <andobernas@gmail.com>
2024-08-10 11:28:01 -0400

    Got fake_gps very close to working

---
+++ scripts/test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import rospy
+import tf2_ros
+from geometry_msgs.msg import PoseStamped, TransformStamped
+
+def pose_callback(pose_msg):
+    # Create a TransformStamped message
+    t = TransformStamped()
+
+    t.header.stamp = rospy.Time.now()
+    t.header.frame_id = "map"  # or "world" depending on your setup
+    t.child_frame_id = "fix"  # frame of your vehicle
+
+    # Copy the position
+    t.transform.translation.x = pose_msg.pose.position.x
+    t.transform.translation.y = pose_msg.pose.position.y
+    t.transform.translation.z = pose_msg.pose.position.z
+
+    # Copy the orientation
+    t.transform.rotation.x = pose_msg.pose.orientation.x
+    t.transform.rotation.y = pose_msg.pose.orientation.y
+    t.transform.rotation.z = pose_msg.pose.orientation.z
+    t.transform.rotation.w = pose_msg.pose.orientation.w
+
+    # Publish the transform to the /mavros/fake_gps/mocap/tf topic
+    tf_pub.publish(t)
+
+def main():
+    rospy.init_node('mocap_to_tf_publisher')
+
+    # Publisher to the /mavros/fake_gps/mocap/tf topic
+    global tf_pub
+    tf_pub = rospy.Publisher('/mavros/fake_gps/mocap/tf', TransformStamped, queue_size=10)
+
+    # Subscribe to the mocap pose topic
+    rospy.Subscriber('/mavros/vision_pose/pose', PoseStamped, pose_callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
+
