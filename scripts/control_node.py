ca3873fa9e2c230e47b0a1202cf62c02339f417a
bandofpv <andobernas@gmail.com>
2024-08-17 14:40:03 -0400

    Removed VIO dockers and revised control_node.py gps origin and home methods

--- scripts/control_node.py
+++ scripts/control_node.py
@@ -2,123 +2,60 @@ import tf
 import math
 import rospy
 from pymavlink import mavutil
+from sensor_msgs.msg import NavSatFix, NavSatStatus
+from geographic_msgs.msg import GeoPointStamped
 from mavros_msgs.msg import State, ExtendedState
 from geometry_msgs.msg import Pose, PoseStamped, Twist, Quaternion
-from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
-
-# TESTING
-from geographic_msgs.msg import GeoPointStamped
-from mavros_msgs.srv import CommandHome, NavSatFix
+from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest, CommandHome, SetHomeRequest
 
 class MavrospyController:
     """
     Controller class to help interface with mavros
     """
-    def __init__(self, send_rate):
-        # initialize our control node
+    def __init__(self, frequency):
+        # Initialize our control node
         rospy.init_node("mavrospy_node")
 
-        # create subscribers
-        rospy.Subscriber("mavros/state", State, self.state_callback)
+        # Create subscribers
+        rospy.Subscriber("/mavros/state", State, self.state_callback)
         rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.pose_callback)
         rospy.Subscriber("/mavros/extended_state", ExtendedState, self.extended_state_callback)
+        rospy.Subscriber('/mavros/global_position/gp_origin', GeoPointStamped, self.origin_callback)
+        rospy.Subscriber('/mavros/global_position/global', NavSatFix, self.gps_callback)
 
-        # create publishers
+        # Create publishers
         self.cmd_pos_pub = rospy.Publisher("/mavros/setpoint_position/local", PoseStamped, queue_size=1)
+        self.origin_pub = rospy.Publisher('/mavros/global_position/set_gp_origin', GeoPointStamped, queue_size=10)
 
-        # create services
+        # Create services
         self.mode_service = rospy.ServiceProxy('/mavros/set_mode', SetMode)
         self.arm_service = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+        self.set_home_service = rospy.ServiceProxy('/mavros/cmd/set_home', CommandHome)
 
-        # initialize ROS messages
+        # Initialize ROS messages
         self.pose = Pose()
         self.current_state = State()
         self.timestamp = rospy.Time()
         self.current_extended_state = ExtendedState()
 
-        # initialize constants
+        # Initialize constants
         self.pi_2 = math.pi / 2.0
-        self.freq = send_rate
-        self.rate = rospy.Rate(send_rate)
-
-        # TESTING
-        self.origin_pub = rospy.Publisher('/mavros/global_position/set_gp_origin', GeoPointStamped, queue_size=10)
-        self.origin_sub = rospy.Subscriber('/mavros/global_position/gp_origin', GeoPointStamped, self.origin_callback)
-        self.origin_set = False
+        self.freq = frequency
+        self.rate = rospy.Rate(frequency)
         self.target_lat = 38.9853504  # Latitude of the Hopper Hall
         self.target_lon = -76.4857648  # Longitude of the Hopper Hall
         self.target_alt = 36.810  # Altitude of the Hopper Hall
-        try:
-            self.run()
-        except rospy.ROSInterruptException:
-            pass
 
+        # Status variables
+        self.origin_set = False
         self.current_gps = None
 
-        # Subscribe to the current GPS topic
-        rospy.Subscriber('/mavros/global_position/global', NavSatFix, self.gps_callback)
-
-        self.set_home_service = rospy.ServiceProxy('/mavros/cmd/set_home', CommandHome)
+        # Set global origin and home position
+        self.set_global_origin()
+        self.wait_for_gps_fix()
         self.set_home_position()
 
-        rate = rospy.Rate(1)  # 1 Hz
-        while not rospy.is_shutdown():
-            self.set_home_position()
-            if self.set_home_position():
-                rospy.loginfo("Home position successfully set.")
-                break
-            rate.sleep()
-
         self.log_info("MavrospyController Initiated")
-
-    def set_global_origin(self):
-        origin_msg = GeoPointStamped()
-        origin_msg.header.stamp = rospy.Time.now()
-        origin_msg.header.frame_id = "map"  # Use "map" as the coordinate reference
-
-        origin_msg.position.latitude = self.target_lat
-        origin_msg.position.longitude = self.target_lon
-        origin_msg.position.altitude = self.target_alt
-
-        rospy.sleep(1)  # Wait for the publisher to connect
-        self.origin_pub.publish(origin_msg)
-
-    def origin_callback(self, msg):
-        if msg.position.latitude == self.target_lat and msg.position.longitude == self.target_lon:
-            self.origin_set = True
-            rospy.loginfo("Global origin confirmed.")
-        else:
-            rospy.logwarn("Global origin mismatch: Expected (%.6f, %.6f) but got (%.6f, %.6f)",
-                          self.target_lat, self.target_lon, msg.position.latitude, msg.position.longitude)
-
-    def run(self):
-        rospy.loginfo("Setting global origin...")
-        rate = rospy.Rate(1)  # 1 Hz
-
-        while not rospy.is_shutdown():
-            self.set_global_origin()
-            if self.origin_set:
-                rospy.loginfo("Global origin successfully set and verified.")
-                break
-            rate.sleep()
-
-    def gps_callback(self, data):
-            self.current_gps = data
-
-    def set_home_position(self):
-            if self.current_gps is not None:
-                try:
-                    # Set the current GPS position as home
-                    response = self.set_home_service(current_gps=True, latitude=0, longitude=0, altitude=0)
-                    if response.success:
-                        rospy.loginfo("Home position set successfully!")
-                        return True
-                    else:
-                        rospy.logwarn("Failed to set home position.")
-                except rospy.ServiceException as e:
-                    rospy.logerr("Service call failed: %s" % e)
-            else:
-                rospy.logwarn("Current GPS location is not available yet.")
 
     def state_callback(self, data):
         """
@@ -138,6 +75,23 @@ class MavrospyController:
         """
         self.timestamp = data.header.stamp  # timestamp pose message
         self.pose = data.pose
+
+    def origin_callback(self, data):
+        """
+        Callback for global origin data
+        """
+        # Check if global origin is set properly
+        if data.position.latitude == self.target_lat and data.position.longitude == self.target_lon:
+            self.origin_set = True
+        else:
+            rospy.logwarn("Global origin mismatch: Expected (%.6f, %.6f) but got (%.6f, %.6f)",
+                          self.target_lat, self.target_lon, data.position.latitude, data.position.longitude)
+
+    def gps_callback(self, data):
+        """
+        Callback for GPS data
+        """
+        self.current_gps = data
 
     def log_info(self, info):
         """
@@ -159,7 +113,7 @@ class MavrospyController:
         """
         try:
             self.rate.sleep()
-        except rospy.ROSException as e:
+        except rospy.ROSInterruptException as e:
             self.log_error(e)
 
     def service_request(self, service, request):
@@ -183,6 +137,59 @@ class MavrospyController:
 
         except rospy.ServiceException as e:
             self.log_error(e)
+
+    def set_global_origin(self):
+        """
+        Set the global origin
+        """
+        def send_origin_msg():
+            # Setup GeoPointStamped message
+            origin_msg = GeoPointStamped()
+            origin_msg.header.stamp = rospy.Time.now()
+            origin_msg.header.frame_id = "map"  # Use "map" as the coordinate reference
+
+            # Assign lat, long, and alt to the message
+            origin_msg.position.latitude = self.target_lat
+            origin_msg.position.longitude = self.target_lon
+            origin_msg.position.altitude = self.target_alt
+
+            # Publish GeoPointStamped message
+            try:
+                self.origin_pub.publish(origin_msg)
+            except rospy.ROSException as e:
+                self.log_error(e)
+
+        self.log_info("Setting global origin...")
+
+        # Keep sending global origin message until it is set properly
+        while not rospy.is_shutdown():
+            send_origin_msg()
+            if self.origin_set:
+                self.log_info("Global origin set.")
+                break
+            self.pause()
+
+    def wait_for_gps_fix(self):
+        """
+        Wait for GPS fix
+        """
+        self.log_info("Waiting for GPS fix...")
+
+        while not rospy.is_shutdown():
+            if self.current_gps.status.status == NavSatStatus.STATUS_FIX:
+                self.log_info("GPS fix acquired.")
+                break
+            self.pause()
+
+    def set_home_position(self):
+        """
+        Set the home position
+        """
+        rospy.loginfo("Setting home position...")
+
+        # Set the current GPS position as home
+        self.service_request(self.set_home_service, SetHomeRequest(current_gps=True, latitude=0, longitude=0, altitude=0))
+        rospy.loginfo("Home position set.")
 
     def arm(self, status, timeout=5):
         """
